ea82aa8ae4a6ad8fc9ba359f2312a748a133412e
Haider Abbas <haiderabbas@Samreens-MacBook-Pro.local>
2019-04-22 15:26:40 +0200

    setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const express = require('express');
 const hbs = require('hbs')
 
 const app = express();
+const port =  process.env.PORT || 3000
 
 //define paths for express config
 const publicDirectory = path.join(__dirname,'../public')
@@ -90,6 +91,6 @@ app.get('*', (req, res) => {
 
 
 
-app.listen(3000, () =>{
+app.listen(port, () =>{
     console.log('server is up on port 3000')
 })
