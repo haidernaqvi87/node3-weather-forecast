4db6cd66b6d9c59c9c0fc512414a777943de99ca
Haider Abbas <haiderabbas@Samreens-MacBook-Pro.local>
2019-04-22 15:36:44 +0200

    a minor string change

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (lat,long,callback) => {
             callback('Unable to find the forecast', undefined);
         }
         else{
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain. Thank you for using our app.')
                }
     })
 }
